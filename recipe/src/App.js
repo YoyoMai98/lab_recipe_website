bb892ede829aeb555218acd6e3d39be684ccb51a
Yongran <maiyongran@gmail.com>
2022-09-29 14:49:32 +0100

    create Title component

--- recipe/src/App.js
+++ recipe/src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Title from './components/Title';
 
 function App() {
   return (
     <div>
+      <Title />
     </div>
   );
 }
---
+++ recipe/src/components/Title.js
@@ -0,0 +1,9 @@
+const Title = () => {
+    return (
+        <div className="title">
+            Food Recipe
+        </div>
+    )
+}
+
+export default Title
