3b6ea358cadb4bf087c9c9944fcae5019915db15
Yongran <maiyongran@gmail.com>
2022-09-29 14:54:29 +0100

    create Navigation

--- recipe/src/App.js
+++ recipe/src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import Title from './components/Title';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
     <div>
       <Title />
+      <ul>
+        <Navigation />
+      </ul>
     </div>
   );
 }
---
+++ recipe/src/components/Navigation.js
@@ -0,0 +1,12 @@
+const Navigation = () => {
+    return (
+        <nav className="navigation">
+            <a href="#">Recipes</a>
+            <a href="#">How to cook</a>
+            <a href="#">Inspiration</a>
+            <a href="#">Reviews</a>
+        </nav>
+    )
+}
+
+export default Navigation
